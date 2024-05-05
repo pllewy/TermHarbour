8a35556e288d12224c7bc32a3be8936376fedb6e
s444427 <pllewyx@gmail.com>
2024-05-05 16:53:56 +0200

    Added integration with AI to process pdf files into glossaries

--- static/upload.js
+++ static/upload.js
@@ -9,19 +9,29 @@ function uploadFile() {
     const formData = new FormData();
     formData.append("file", file);
 
-    fetch("/upload", {
+    fetch("/test", {
       method: "POST",
       body: formData,
     })
       .then((response) => {
         if (response.ok) {
-          console.log("File uploaded successfully");
+          console.log("File uploaded xsaxxsa successfully");
+          return response.json();
           // Optionally, display a success message to the user
         } else {
           console.error("Failed to upload file");
           // Optionally, display an error message to the user
         }
-      })
+      }).then((resp) => {
+        const content = resp.content;
+        console.log(content)
+
+      let text = "";
+for (let i = 0; i < content.length; i++) {
+  text += content[i] + "<br>";
+}
+      document.getElementById('added_content').innerHTML = text
+    })
       .catch((error) => {
         console.error("Error:", error);
         // Optionally, display an error message to the user
